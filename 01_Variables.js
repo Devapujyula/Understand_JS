83f9111d869ee399ed30fb0d682bddee92e15e6d
Devapujyula <sdevapujyula@gmail.com>
2024-01-01 00:53:23 +0530

    added variables code in js

---
+++ 01_Variables.js
@@ -0,0 +1,12 @@
+// Letters, Digits, Underscores and $ signs are not allowed
+// must begin with a $, -, or a letter
+// JavaScript reserved words cannot be used as a variable name
+// Prakash & praKAsh are different variables(Case Sensitive)
+
+// Declaring a variable
+let a = 45;
+console.log(a);
+
+a = "Prasad";
+
+console.log(a);
