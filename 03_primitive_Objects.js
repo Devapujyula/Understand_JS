48975886a0019de977d9ccfb8db6110e4768d4a9
Devapujyula <sdevapujyula@gmail.com>
2024-01-03 22:07:00 +0530

    added primitive objects code

---
+++ 03_primitive_Objects.js
@@ -0,0 +1,15 @@
+// Primitve JavaSciprt are a set of basic data types in JS
+// Object is a non primitive data types in JS
+//
+// Trick to remember dataTypes, 7DT are => N N B B S S U
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567") + BigInt("45");
+let e = "Prakash";
+let f = Symbol("I am a nice symbol");
+let g = undefined;
+
+console.log(a, b, c, d, e, f, g);
+console.log(typeof d);
