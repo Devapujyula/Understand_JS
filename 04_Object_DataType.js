fc72bcce7407f38c40a2f260548ed3cae1d9c1fc
Devapujyula <sdevapujyula@gmail.com>
2024-01-04 11:02:13 +0530

    added objects data types code in js

---
+++ 04_Object_DataType.js
@@ -0,0 +1,13 @@
+// An Object in a JavaScript can be created as follows
+
+const Item = {
+  firstName: "Prakash",
+  lastname: "Dsp",
+  age: 25,
+  salary: null,
+  isActive: false,
+  status: undefined,
+};
+
+console.log(Item);
+console.log(Item["salary"]);
