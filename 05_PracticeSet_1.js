3c42f278c84a352686a844e75a34ea249ec1ae7f
Devapujyula <sdevapujyula@gmail.com>
2024-01-05 21:19:26 +0530

    added practice set

---
+++ 05_PracticeSet_1.js
@@ -0,0 +1,22 @@
+var firstName = "Prasanna";
+var item = 6;
+
+console.log(firstName + item);
+
+console.log(typeof firstName);
+
+const itemsName = "Carrot";
+
+// itemsName = "Potato";
+
+console.log(itemsName);
+
+let basicEnglishBook = {
+  Prakash: "name",
+  Potato: "veggi",
+  Pen: "article",
+  Flipkart: "Application",
+  Laptop: "Accessories",
+};
+
+console.log(basicEnglishBook);
