575311f7269f7d3e56e99b51dbb74b0f0d2c6b40
Devapujyula <sdevapujyula@gmail.com>
2024-01-06 18:28:59 +0530

    added arthimetic operators in js

---
+++ 06_Arithematic_Operators.js
@@ -0,0 +1,24 @@
+// A fragments of code that produces a value is called Expression
+console.log("Operators in JS");
+let a = 45;
+let b = 9;
+// Addition
+console.log("a + b = ", a + b);
+// Subtraction
+console.log("a - b = ", a - b);
+// Multiplication
+console.log("a * b = ", a * b);
+// Division
+console.log("a / b = ", a / b);
+// Modulo
+console.log("a % b = ", a % b);
+// Power
+console.log("a ** b = ", a ** b);
+// Post Increment
+console.log("a++ =", a++);
+// Pre Increment
+console.log("++a =", ++a);
+// Post Decrement
+console.log("a-- =", a--);
+// Pre Decrement
+console.log("--a =", --a);
