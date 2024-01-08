7b4c7e0f665995e9b5f1901b57731b2965a2713f
Devapujyula <sdevapujyula@gmail.com>
2024-01-08 11:40:35 +0530

    added assignment operator code in js

---
+++ 07_Assignment_Operators.js
@@ -0,0 +1,24 @@
+let a = 45;
+a = 40;
+console.log(a);
+
+a += 50;
+console.log(a);
+
+let b = 30;
+console.log(b);
+
+b -= 20;
+console.log(b);
+
+let c = 50;
+console.log(c);
+
+c = -30;
+console.log(c);
+
+let d = 4;
+
+d /= 2;
+
+console.log(d);
