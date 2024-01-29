4b8896dc8c62e9e844620fbb5292b6bad58e7aa0
Devapujyula <sdevapujyula@gmail.com>
2024-01-29 17:32:03 +0530

    added if conditional in js

---
+++ 10_if_Conditional.js
@@ -0,0 +1,12 @@
+// if statement
+// if.. esle statement
+// if else if statement
+
+let a = prompt("What's your age?");
+a = Number.parseInt(a); // Converting the string to a number
+console.log(typeof a);
+if (a > 0) {
+  alert("This is a valid age");
+} else {
+  alert("This is an invalid age");
+}
