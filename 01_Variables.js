f31575a3d85c4b3550834d54b4b786eb4cadd075
Devapujyula <sdevapujyula@gmail.com>
2024-02-01 18:56:35 +0530

    updated js code

--- 01_Variables.js
+++ 01_Variables.js
@@ -1,12 +1,14 @@
 // Letters, Digits, Underscores and $ signs are not allowed
-// must begin with a $, -, or a letter
+// must begin with a $, _(underscroll), or a letter
 // JavaScript reserved words cannot be used as a variable name
 // Prakash & praKAsh are different variables(Case Sensitive)
-
+// variables - Store, Access, Modify === value
+// these are case sensitive
 // Declaring a variable
+
 let a = 45;
 console.log(a);
 
-a = "Prasad";
+a = "Sowmya";
 
 console.log(a);
