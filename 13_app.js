79046420fe170cb82e2665abebdb32fcd537b309
Devapujyula <sdevapujyula@gmail.com>
2024-02-04 19:18:11 +0530

    added js code

---
+++ 13_app.js
@@ -0,0 +1,11 @@
+// document.querySelectorAll(".btn").forEach((item) => {
+//   item.addEventListener("click", () => {
+//     alert(
+//       "Hello welcome to Home page, Home page under Designer for fantastic stuff he is working on it"
+//     );
+//   });
+// });
+
+document.write("Hello world...!");
+alert("Hello People..!");
+console.log("hello world..!");
