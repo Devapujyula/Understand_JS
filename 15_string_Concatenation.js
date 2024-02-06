490af79cecc010a97e30c9983883182af641961f
Devapujyula <sdevapujyula@gmail.com>
2024-02-06 16:31:33 +0530

    added string concatination code

---
+++ 15_string_Concatenation.js
@@ -0,0 +1,12 @@
+// String Concatenation - combine string values
+// `` - backticks (template strings) easier option
+
+// const firstName = "Prakash";
+// const lastName = "Devapujyula";
+
+// let fullName = firstName + " " + lastName;
+// console.log("Hello there your full name is : " + fullName);
+
+const website = "google";
+const url = "https://www. " + website + ".com";
+console.log(url);
