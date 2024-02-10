1b227c0ee1b9f75a8c280aff62638ece99aa4105
Devapujyula <sdevapujyula@gmail.com>
2024-02-10 21:19:16 +0530

    added numbers js code

---
+++ 17_Numbers.js
@@ -0,0 +1,24 @@
+// Numbers
+// Loosely typed = don't care declare type
+
+// const noOfPants = 45;
+// let priceOfPants = 1245.678;
+// priceOfPants = 1235.854;
+
+// console.log(noOfPants);
+// console.log(priceOfPants);
+
+let number = 40;
+number += 5;
+number += 5;
+number += 5;
+number -= 5;
+
+const slices = 10;
+const children = 3;
+
+const amount = slices % children;
+
+console.log(amount);
+
+console.log(number);
