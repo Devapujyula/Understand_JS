6a414859a500e9ab834261b0380da062f04f234e
Devapujyula <sdevapujyula@gmail.com>
2024-02-11 21:15:57 +0530

    added challenge

---
+++ 18_Challenge_Numbers.js
@@ -0,0 +1,22 @@
+let score1 = 20;
+let score2 = 40;
+let score3 = 30;
+
+const totalScore = score1 + score2 + score3;
+const averageScore = totalScore / 3;
+
+console.log(totalScore);
+console.log(averageScore);
+
+const plates = 20;
+let people = 7;
+
+let remainingPlates = 20 % 7;
+
+// const addingPlates = remainingPlates + 1;
+
+remainingPlates++;
+
+const message = "There are " + remainingPlates + " " + "plates available";
+
+console.log(message);
